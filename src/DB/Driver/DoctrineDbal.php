5b92e59d29fddfd0951fd9cc0ace5696ee18d400
Aaron J. Lang <aaronjameslang@users.noreply.github.com>
2016-08-16 15:25:25 +0100

    Comment out unknown function

--- src/DB/Driver/DoctrineDbal.php
+++ src/DB/Driver/DoctrineDbal.php
@@ -245,11 +245,11 @@ class DoctrineDbal extends Common
         }
 
         // @todo: doctrine? ensure we're using mysql?
-        if (!$this->options['result_buffering']) {
-            $this->connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-        } else {
-            $this->connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
-        }
+        //if (!$this->options['result_buffering']) {
+        //    $this->connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+        //} else {
+        //    $this->connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+        //}
 
         $result = $this->connection->query($query);
 
